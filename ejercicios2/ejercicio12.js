c5836e50d4cbc6d4e86f312bc5c2ddd399c9ca72
santiago123-dex <santiagofajardomorales17@gmail.com>
2025-02-19 18:18:50 -0500

    ejercicios nuevos

---
+++ ejercicios2/ejercicio12.js
@@ -0,0 +1,24 @@
+let modelo = Number(prompt("Ingrese el modelo de su automovil"))
+
+switch(modelo){
+    case 119 :
+        alert("el automóvil esta defectuoso, llevar a garantía")
+        break
+    case 179 :
+        alert("el automóvil esta defectuoso, llevar a garantía")   
+        break
+    case 189 :
+        alert("el automóvil esta defectuoso, llevar a garantía")
+        break
+    case 195 :
+        alert("el automóvil esta defectuoso, llevar a garantía")
+        break
+    case 221 :
+        alert("el automóvil esta defectuoso, llevar a garantía")
+        break
+    case 780 :
+        alert("el automóvil esta defectuoso, llevar a garantía")
+        break
+    default:
+        alert("Su automóvil no está defectuoso")
+}
