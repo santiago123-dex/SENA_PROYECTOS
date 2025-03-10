7d2643901927eda5666f194a9fe8322563c269a0
santiago123-dex <santiagofajardomorales17@gmail.com>
2025-03-10 07:04:46 -0500

    arrays nuevos

--- arrays/ejem.js
+++ arrays/ejem.js
@@ -1,4 +1,20 @@
 
+
 let nombre = ["Juan", "Jose", "Nelson"] // array tipo String
 let numero = [1, 2, 3, 5, 6] // array tipo Numerico
+nombre[2] = "Santi"
+
+
+
+//Propiedades length
+
+let nombre1 = ["Juan", "Jose", "Nelson"] // array tipo String
+// Se agrega en nombre de ultimo en este caso sería despues de Nelson
+nombre1[nombre1.length] = "Ana"
+// En este caso Ana reemplaza a Jose
+nombre1[nombre1.length - 3] = "Ana"
+// muestra cuantos valores tenemos dentro del array
+console.log(nombre1)
+console.log(nombre1.length)
+
 
